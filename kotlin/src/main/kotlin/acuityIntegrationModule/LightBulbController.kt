b8ed273e7c04f2fc6b3e7e0040e414e35ab8e166
Luiz Socrate <luiz.socrate@airahome.com>
2023-08-14 13:47:18 +0200

    Integration simulator

--- kotlin/src/main/kotlin/acuityIntegrationModule/LightBulbController.kt
+++ kotlin/src/main/kotlin/acuityIntegrationModule/LightBulbController.kt
@@ -1,25 +1,28 @@
 package acuityIntegrationModule
 
-import java.util.Random
 import java.util.UUID
 import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 class LightBulbController {
     suspend fun turnBulbOn(id: UUID): String {
-        delay(250)
+        mimicNotFound(id)
+        mimicDelay()
 
-
-        if (Random().nextDouble() < 0.1) throw Error("This bulb does not exist")
-        if (Random().nextDouble() < 0.1) throw Error("This bulb is not working")
+        if (Random.nextDouble() < 0.1) throw Error("This bulb is not working")
 
         return "OK"
     }
 
     suspend fun turnBulbOff(id: UUID): String {
-        delay(250)
-
-        if (Random().nextDouble() < 0.1) throw Error("This bulb does not exist")
-
+        mimicNotFound(id)
+        mimicDelay()
         return "OK"
+    }
+
+    private suspend fun mimicDelay() = delay(Random.nextLong(2, 400))
+
+    private fun mimicNotFound(id: UUID) {
+        if (id.toString()[0] == '0') throw Error("This bulb does not exist")
     }
 }
